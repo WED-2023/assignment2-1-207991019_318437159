f246ec13f507f1579c37531eb982ba38d7421c9f
Tom Sheinman <toms@remit.co.il>
2024-06-05 20:40:01 +0300

    registration-pages

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
+import Navbar from "./components/Navbar.vue";
 
 export default {
   name: "App",
   components: {
-    Navbar
-  }
+    Navbar,
+  },
 };
 </script>
 
